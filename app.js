f4c30d67c426da4b1afe2ef7d87a48c7c35efb9d
pankaj1980patel <pantherpatel1980@gmail.com>
2023-09-11 00:34:40 +0530

    made connection to mongo atlas

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { createProduct, getProducts } = require("./mongo");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/products",getProducts);
+app.post("/products",createProduct);
+
+app.listen(3500);
---
+++ mongo.js
@@ -0,0 +1,49 @@
+const { MongoClient } = require("mongodb");
+
+const userName = "panther1980";
+const password = "JUAoJHSzpEmov9a3";
+const dbName = "products_test";
+
+const url = `mongodb+srv://${userName}:${password}@cluster0.aethqem.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+let client;
+
+// (async () => {
+//   try {
+//     client = new MongoClient(url);
+//     await client.connect();
+//     console.log("Connected to MongoDB");
+
+//     // Your application logic here
+
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//   }
+// })();
+
+const createProduct = async (req, res, next) => {
+  console.log("i am inside createProduct");
+  let newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  client = new MongoClient(url);
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db();
+    const result = await db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Could not store data" });
+  }
+  client.close();
+  res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {
+  // Implement your logic for retrieving products here
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
